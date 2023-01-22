9fa01d967b67715c75eccc3bbf7bb8c563cec82f
gura <csgura@gmail.com>
2023-01-22 22:40:59 +0900

    module override

--- src/module.rs
+++ src/module.rs
@@ -29,3 +29,33 @@ macro_rules! combine_module {
         $crate::CombinedModule::new(vec![$(Arc::new($m),)*])
     };
 }
+
+pub struct OverridableModule {
+    overriden: Vec<Arc<dyn AbstractModule>>,
+}
+
+impl AbstractModule for OverridableModule {
+    fn config(&self, binder: &mut Binder) {
+        self.overriden.iter().for_each(|m| m.config(binder))
+    }
+}
+
+pub struct OverridedModule {
+    overriden: Vec<Arc<dyn AbstractModule>>,
+    overrides: Vec<Arc<dyn AbstractModule>>,
+}
+
+impl AbstractModule for OverridedModule {
+    fn config(&self, binder: &mut Binder) {
+        todo!()
+    }
+}
+
+impl OverridableModule {
+    fn with(&self, overrides: Vec<Arc<dyn AbstractModule>>) -> OverridedModule {
+        OverridedModule {
+            overriden: self.overriden.clone(),
+            overrides,
+        }
+    }
+}
